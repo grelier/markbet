8968dcca9ad88584bb5cebf9354cd7fe08981d7f
florianbgt <florian@gutstickets.org>
2022-06-25 22:01:00 +0200

    SEO 100

--- app.vue
+++ app.vue
@@ -7,4 +7,15 @@
 </template>
 
 <script setup lang="ts">
+useHead({
+  title: 'Live your bet',
+  viewport: 'width=device-width, initial-scale=1',
+  charset: 'utf-8',
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { name: 'description', content: 'Browse and buy bets from the best bettors in town' }
+  ],
+})
 </script>
